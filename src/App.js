8ae6c6c718e1d08b1c9038c42c9e3ac52c92f7df
Gurgen <mkrtchyangurgen8@mail.ru>
2022-02-12 18:11:01 +0400

    chart pie

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import React  from 'react';
+// import Chart from "react-apexcharts"
+
+
+// function App() {
+
+//   const opions={
+//     series:[2,4,5],
+//     labels:["red","yellow","blue"]
+
+//   }
+//   const series=[2,3,4]
+  
+//   return (
+//     <>
+//       <Chart
+//        options={opions}
+//        width="100%"
+//        type="donut"
+//        height={300}
+//        series={series}
+      
+      
+//       />
+
+     
+//     </>
+//   );
+// }
+
+// export default App;
